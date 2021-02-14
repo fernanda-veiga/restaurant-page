b61d82d5a75b69041af47bfaabdd66cde9055f32
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-02-14 19:50:35 -0300

    Added responsivity.

--- src/homepage.js
+++ src/homepage.js
@@ -37,10 +37,7 @@ function generateHomepage() {
     image2.setAttribute("src", "https://images.unsplash.com/photo-1547607700-14bf9e038209?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fGN1cGNha2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60");
     image2.setAttribute("id", "image2");
 
-    let circle = document.createElement("div");
-    circle.setAttribute("id", "circle");
-
-    imageDiv.appendChild(circle);
+    
     imageDiv.appendChild(image1);
     imageDiv.appendChild(image2);
 
@@ -49,30 +46,5 @@ function generateHomepage() {
 
     return homepageContent;
 }
-
-
-
-/*function generateSocialMediaLinks() {
-    let socialMediaContainer = document.createElement("div");
-    socialMediaContainer.classList.add("social-media-links");
-
-    let facebookLink = document.createElement("div");
-    facebookLink.classList.add("social-media");
-    facebookLink.innerHTML = '<a href=#><i class="fab fa-facebook-f"></i></a>';
-
-    let instagramLink = document.createElement("div");
-    instagramLink.classList.add("social-media");
-    instagramLink.innerHTML = '<a href=#><i class="fab fa-instagram"></i></a>';
-
-    let twitterLink = document.createElement("div");
-    twitterLink.classList.add("social-media");
-    twitterLink.innerHTML = '<a href=#><i class="fab fa-twitter"></i></a>';
-
-    socialMediaContainer.appendChild(facebookLink);
-    socialMediaContainer.appendChild(instagramLink);
-    socialMediaContainer.appendChild(twitterLink);
-
-    return socialMediaContainer;
-}*/
 
 export {generateHomepage}
